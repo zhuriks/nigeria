07746ba998590fa7caa55f13e247574dc8de7d79
Zhuriks <ralfs2208@gmail.com>
2019-05-14 10:35:46 +0300

    nibber uno

---
+++ nigeria.js
@@ -0,0 +1,43 @@
+function grade (atz){
+    var grade = console.readLine();
+    if(grade == 1)
+    {
+      console.log(grade+"🤨")
+    }
+    else if(grade == 2)
+    {
+      console.log(grade+"🤨 ")
+    }
+    else if(grade == 3)
+    {
+      console.log(grade+"😣")
+    }
+    else if(grade == 4)
+    {
+      console.log(grade+"🤨 ")
+    }
+    else if(grade == 5)
+    {
+      console.log(grade+"🤨")
+    }
+    else if(grade == 6)
+    {
+      console.log(grade+"🤨")
+    }
+    else if(grade == 7)
+    {
+      console.log(grade+"🤨")
+    }
+    else if(grade == 8)
+    {
+      console.log(grade+"🤩")
+    }
+    else if(grade == 9)
+    {
+      console.log(grade+"🤨")
+    }
+    else if(grade == 10)
+    {
+      console.log(grade+"🤨")
+    }
+    }
